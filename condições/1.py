54f06323b3cc2e43de26364255ee08fa509ea7bc
dudasaanches <124930071+dudasaanches@users.noreply.github.com>
2023-05-26 09:07:41 -0300

    Add files via upload

--- condições/1.py
+++ condições/1.py
@@ -1,5 +1,5 @@
-n1 = int(input("Nota da primeira prova: "))
-n2 = int(input("Nota da segunda prova: "))
+n1 = float(input("Nota da primeira prova: "))
+n2 = float(input("Nota da segunda prova: "))
 
 media = (n1+n2) /2
 if media >= 6:
